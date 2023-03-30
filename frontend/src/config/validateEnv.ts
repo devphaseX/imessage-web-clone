dd7dee8c02a365715025726ae3fc313facf50b86
Ayomide <ayomidelawal700@gmail.com>
2023-03-30 10:28:01 +0100

    implement google auth

--- frontend/src/config/validateEnv.ts
+++ frontend/src/config/validateEnv.ts
@@ -4,9 +4,15 @@ const envSchema = z.object({
   GOOGLE_CLIENT_ID: z.string().min(32),
   GOOGLE_CLIENT_SECRET: z.string().min(8),
   DATABASE_URL: z.string(),
+  NEXTAUTH_URL: z.string(),
+  NEXTAUTH_SECRET: z.string(),
 });
 
-const parseEnvironmentVars = envSchema.parse(process.env) as NodeJS.ProcessEnv;
+const parseEnvironmentVars =
+  typeof window !== 'undefined'
+    ? ({} as NodeJS.ProcessEnv)
+    : (envSchema.parse(process.env) as NodeJS.ProcessEnv);
+
 type ZodEnvType = typeof envSchema._output;
 
 declare global {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,18 @@
 import type { GetServerSideProps } from 'next';
 import '../config/validateEnv';
+import { signIn, useSession, signOut } from 'next-auth/react';
 
 export default function Home() {
+  const { data } = useSession();
+  console.log('session data', data);
   return (
     <>
-      <div>Hello from next.js</div>
+      <button type="button" onClick={() => signIn('google')}>
+        Sign in
+      </button>
+      <button type="button" onClick={() => signOut()}>
+        Signout
+      </button>
     </>
   );
 }
